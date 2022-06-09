ac146ae39a7ec2ae5c09a36560fcc96bbfb3b2c9
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-06-09 12:18:47 +0300

    commit tree 319a8eae2fe4613d3e8ff9561563f7c440af0cb3

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+  if (activity) {
+      return `This Saturday, I want to ${activity}!`
+  } else {
+      return `This Saturday, I want to ${activity}!`
+  }
+}
+
+function mondayWork(activity="go to the office") {
+  if (activity) {
+      return `This Monday, I will ${activity}.`;
+  } else {
+      return `This Monday, I will ${activity}.`;
+  }
+}
+
+let wrapAdjective = function(symbol="*") {
+  return function(character="special") {
+      return `You are ${symbol}${character}${symbol}!`;
+  }
+}
